89d232a43fca8f271b4ee81d96388b750ce8d169
Tess Schurder <tschurder0518@gmail.com>
2021-01-30 16:58:38 -0600

    Docs: Added Pre/Post Condition Comments
    Added more notes and comments with the pre-conditions and
    post-conditions in Inventory class.

--- HOMEINVY/src/inventory/Inventory.java
+++ HOMEINVY/src/inventory/Inventory.java
@@ -14,15 +14,23 @@ import items.SoftCopy;
 import items.HardCopy;
 
 public class Inventory {
-
+	/* INTENT: This class creates the inventory of items. It reads
+	 * and parses a .txt file and uses the information to create Item 
+	 * objects for each item in the inventory file. 
+	 * 
+	 * See main() for more information.
+	 */
+	
 	// CONSTANT for file name
 	static String INVENTORY_FILE = "src/inventory/InventoryFile.txt";
 	
+	// CONSTANT for all items created from the file
 	public static ArrayList<Item> theItems = new ArrayList<Item>();
 	
 	public static ArrayList readInventory(String INVENTORY_FILE) throws IOException,
 			InsufficientItemDataException {
 		// Create new Scanner object for the inventory file, read each line
+		// Post1 - read file contents
 		try(Scanner input = new Scanner(Paths.get(INVENTORY_FILE))) {
 			
 			// Check if there is another line of input in the file
@@ -38,18 +46,18 @@ public class Inventory {
 		}
 		// Catch exception for missing elements, so blanks (lacking comma to separate items in line) in file
 		catch(NoSuchElementException e) {
-			throw new InsufficientItemDataException();
-															
+			throw new InsufficientItemDataException();															
 		}
 		return theItems;
-	}
+	} // End readInventory() method
 	
 	private static ArrayList parseData(String str) throws InsufficientItemDataException {
+		// Parse through information and create objects - Post2 and Post3
 		String category, name, location, purchaseYear, condition, att1, att2, att3, att4, att5;
 		Scanner lineScanner = new Scanner(str);
 		lineScanner.useDelimiter(","); // Split line using , as delimiter to separate each attribute
 		while(lineScanner.hasNext()) {
-			// Grab each individual attribute on the line and assign to variable for adding to Item objects
+			// Grab each individual attribute on the line and assign to variable for adding to Item objects - Post2
 			category = lineScanner.next();
 			name = lineScanner.next();
 			location = lineScanner.next();
@@ -63,6 +71,7 @@ public class Inventory {
 			
 			// Check value of category, based on value call constructor for that Item type to create item, then add
 			// to ArrayList of items
+			// Post3
 			if (category.equals("Power Tool")) {
 				Item item = new PowerTool(name, location, purchaseYear, condition, att1, att2);
 				theItems.add(item);
@@ -97,6 +106,18 @@ public class Inventory {
 	
 	
 	 public static void main(String[] args) {
+		/* Precondition 1: InventoryFile.txt contains the desired items to go into inventory.
+		 * The formating is as in a CSV, each item is in a new line, each property for items is
+		 * separated by a comma. Each line must contain 10 commas, including extra commas if
+		 * item has fewer than 9 attributes.
+		 * 
+		 * Postcondition 1 (Import): Importing file using readInventory() method.
+		 * Post2 (Parse Data): Contents of each line assigned to temporary variables for each item
+		 * in file.
+		 * Post3 (Create objects): Each line of information in file is used to create objects, which
+		 * are then added to ArrayList.
+		 * Post4 (Display): The items are displayed on the console.
+		 */		
 		 
 		 try {
 			 theItems = readInventory(INVENTORY_FILE);
@@ -105,7 +126,7 @@ public class Inventory {
 			 System.out.println(e);
 		 }
 		 
-		 
+		// Post4 
 		System.out.println("***** Displaying " + theItems.size() + " items *****");
 		// Iterate through each element of Item type
 		// Determine the instance of each element and print based on that info
